6b46f6b33483d738258bdb261ab9ef818e0c64e1
Tudor Avram <a96tudor@gmail.com>
2017-02-13 17:22:29 +0000

    added website blocker

---
+++ website-blocker/setup.py
@@ -0,0 +1,108 @@
+import platform
+from dateutil import parser as p
+from datetime import datetime as dt
+
+separator = "-"*60
+
+def error_message(msg, terminate = True):
+    """
+        This function prints a given error message
+    :param msg:             The messsage to be printed
+    :param terminate:       True if we want to terminate the program
+                            False if not
+    :return:                -
+    """
+    print(msg)
+    if terminate:
+        exit()
+
+def print_platform(f):
+    """
+        Function that gets the platform ID and prints it to the config file
+    :param: f       The file pointer
+    :return: -
+    """
+
+    known_platforms = [
+        "windows",
+        "linux",
+        "darwin"  # MAC OS X
+    ]
+
+    # getting the operating system
+    _platform = platform.system().lower()
+    # doing the check
+    if not (_platform in known_platforms):
+        # error
+        f.write("OS ERROR\n")  # writing the error to the config file
+        error_message("ERROR: We are sorry, but this application is not available for this operating system")
+    else:
+        f.write(_platform + "\n")
+
+def print_websites(f):
+    """
+        This function reads from the console and prints to the config file the
+    websites that are going to be blocked
+    :param f:       The file pointer to write to
+    :return:    -
+    """
+    # getting websites counter
+    try:
+        cnt = int(input("How many websites do you want to block?\n"))
+    except ValueError:
+        f.write("INPUT ERROR\n")
+        error_message("ERROR: You didn't write a valid number")
+
+    # getting the websites
+    for i in range(cnt):
+        website = input("Enter the website number" + str(i + 1) + ": ")
+        f.write(website.replace("www.", "") + "\n")
+        f.write(website + "\n")
+    f.write("#\n")
+
+def check_format(input):
+    """
+        This function checks if a given string is in a date format. If not, it terminates the program
+    :param input:       the input string
+    :return:            The input as an datetime.datetime object if the string is in the right format
+                        Nothing if it's not
+    """
+    try :
+        return p.parse(input)
+    except ValueError:
+        print("ERROR: The specified date/time is not valid.")
+        exit()
+
+def print_start_and_end_times(f):
+    """
+        This function reads the start and end hours for the blocker and prints them to the config file
+    :param f:   The file pointer
+    :return:    -
+    """
+    print("Now you can set the times you want the websites to be blocked between: \n" +
+          "They have to be in a readable date-time format and mention the hour and minutes"
+        )
+    start = check_format(input("Start time: \n"))
+    end = check_format(input("End time: \n"))
+
+    if start >= end:
+        #error
+        error_message("ERROR: The end time is smaller than the start time.\n")
+
+    str_start = start.strftime("%H:%M")
+    str_end = end.strftime("%H:%M")
+
+    if str_start == "00:00" or str_end == "00:00":
+        error_message("ERROR: The specified times are not valid\n")
+
+    f.write(str_start + "\n")
+    f.write(str_end + "\n")
+
+def main():
+
+    with open("config.cnf", "w") as f:
+        print_platform(f)
+        print_websites(f)
+        print_start_and_end_times(f)
+
+main()
---
+++ website-blocker/website-blocker.py
@@ -0,0 +1,147 @@
+import platform
+from dateutil import parser as p
+from datetime import datetime as dt
+import time
+
+def error_message(msg, terminate = True):
+    """
+        This function prints a given error message
+    :param msg:             The messsage to be printed
+    :param terminate:       True if we want to terminate the program
+                            False if not
+    :return:                -
+    """
+    print(msg)
+    if terminate:
+        exit()
+
+def startup_checks():
+    """
+        Function that does the initial checks (such as if it is running on a supported OS)
+    :return:    - the path to the hosts file
+                - the redirect IP address
+    """
+    hosts_paths = {
+        "windows": r"C:\Windows\System32\drivers\etc\hosts",
+        "linux": r"/etc/hosts",
+        "darwin": r"/etc/hosts"
+    }
+
+    known_platforms = [
+        "windows",
+        "linux",
+        "darwin"  # MAC OS X
+    ]
+
+    # getting the operating system
+    _platform = platform.system().lower()
+
+    # doing the check
+    if not (_platform in known_platforms):
+        # error
+        print("ERROR: We are sorry, but this application is not available for this operating system")
+        exit()  # terminating the current application
+
+    hosts_path = hosts_paths[_platform]
+    redirect = "127.0.0.1"
+
+    return hosts_path, redirect
+
+def get_websites_list(f):
+    """
+        Reads the list of websites from the config files
+    :param f:       The file pointer to read from
+    :return:        The list of websites
+    """
+    websites = list()
+    # creating the websites list
+    line = f.readline()
+    print(line == "#")
+    while not (line == "#\n"):
+        websites.append(line.replace("\n", ""))
+        line = f.readline()
+
+    return websites
+
+def get_start_and_end_times(f):
+    """
+        Reads the start and end times from the config file
+    :param f:       The file pointer to read from
+    :return:        two datetime.datetime objects, representing the
+                start and end times
+    """
+    str_start = f.readline()
+    start = p.parse(str_start)
+    str_end = f.readline()
+    end = p.parse(str_end)
+
+    return start, end
+
+def solve_configs(cnf_file, redirect, host_path):
+    """
+        Reads the configuration file and collects the necessary data
+        Also writes the website
+    :param cnf_file:     The path to the configuration file
+    :param redirect:     The IP address to redirect to
+    :param host_path:    The path to the hosts file
+    :return:     -  Two datetime.datetime objects, representing
+    """
+
+    print("Solving config\n")
+    with open(cnf_file, "r") as f:
+        _ = f.readline()  # ignoring the 1st line of the file
+
+        websites = get_websites_list(f)
+        start, end = get_start_and_end_times(f)
+
+    with open(host_path, "r+") as f:
+        content = f.read()
+        # we have to write all the websites
+        for website in websites:
+            if website in content:
+                pass
+            else:
+                f.write(redirect + " " + website + "\n")
+    print("Written config\n")
+
+    return websites, start, end
+
+def reset_hosts_file(path, websites):
+    """
+        Resets the hosts file, by removing every change made by this program
+    :param path:        The path to the hosts file
+    :param websites:    The websites list
+    :return:            -
+    """
+    with open(path, "r+") as f:
+        contents = f.readlines()
+        f.seek(0) #setting the file pointer
+        for line in contents:
+            if not any(website in line for website in websites):
+                f.write(line)
+        f.truncate()
+def main():
+    """
+        The main function of the program
+    :return: -
+    """
+    hosts, redirect = startup_checks()
+
+    websites, start, end = solve_configs(
+        cnf_file="config.cnf",
+        redirect=redirect,
+        host_path=hosts
+    )
+
+    while True:
+        if start < dt.now() < end:
+            #still work hours
+            print("Still working hours... :(")
+        else:
+            # free time
+            print("Yay!! Working hours are over! :D")
+            reset_hosts_file(websites=websites, path=hosts)
+            #let's reset the hosts file
+        time.sleep(5)
+
+main()
