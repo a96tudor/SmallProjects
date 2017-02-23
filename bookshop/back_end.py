83af1132e64b93aa4a7e1e68bb92cc8f81be97c6
Tudor Avram <a96tudor@gmail.com>
2017-02-23 00:15:40 +0000

    frontend-backend partial integration

---
+++ bookshop/back_end.py
@@ -0,0 +1,130 @@
+import sqlite3 as sql
+
+class Database:
+
+    """
+        A custom database class for the bookshop application
+    """
+
+    def __init__(self, db_name):
+        """
+            CONSTRUCTOR
+        :param db_name:     The name of the database we are going to use
+        """
+        self._dbName = db_name
+        self._check_integrity()
+
+    def _check_integrity(self):
+        """
+            Method that checks the integrity of the database (i.e. if it has the required tables, etc)
+        :return:    -
+        """
+        #setting up the connection
+        con = sql.connect(self._dbName)
+        cur = con.cursor()
+
+        #Creating the table, if it doesn't exist. This way we make sure that we have it in the correct format
+        query = "CREATE TABLE IF NOT EXISTS " \
+            "book (" \
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," \
+                "title VARCHAR(50) ," \
+                "author VARCHAR(50)," \
+                "'year' INT," \
+                "isbn VARCHAR(13)" \
+            ")"
+        cur.execute(query)
+        con.commit()
+
+        #closing the connection
+        con.close()
+
+    def fetch(self, limit=-1, conds=dict()):
+        """
+            Method that reads all the entries from the table
+        :param limit:       How many elements to fetch from the table
+
+                Default: -1 = no limit (i.e. fetching all of them)
+        :param condition:   The conditions used when we run the query
+                    It is a dictionary with the format :
+                        <column_name> : <condition>
+
+                Default: {} = no conditions
+
+        :return:    The entries from the table, as a list of tuples
+        """
+
+        l_conds=list()
+        #Setting up the conditions
+        for col in conds:
+            l_conds.append(col + "=" + conds[col])
+        condition=" AND ".join(l_conds)
+        if condition!="":
+            condition = " WHERE " + condition
+
+        #setting up the query
+        query = "SELECT * FROM book" + condition
+        if limit != -1:
+            query += " LIMIT " + str(limit)
+
+        #opening the connection
+        con = sql.connect(self._dbName)
+        cur = con.cursor()
+
+        #running the query
+        cur.execute(query)
+        results = cur.fetchall()
+
+        #closing the connection
+        con.close()
+
+        return results
+
+    def _get_entry(self, d, key):
+        """
+
+        :param d:       The dictionary
+        :param key:     The key of the entry we want from the dictionary
+
+        :return:         - The value at that specific key, if the key exists in the dictionary
+                         - "NULL", otherwise
+        """
+        if key in d:
+            return d[key]
+        else:
+            return "NULL"
+
+    def insert(self, inputs):
+        """
+
+        :param inputs:      the new entries to be inserted into the db table, as a dictionary
+                The dictionary has to be of the format :
+                        <column_name> : <value>
+
+        :return:    True : if the insert was successful
+                    False : otherwise
+        """
+        #getting the data from the dictionary
+        title = self._get_entry(inputs, "title")
+        author = self._get_entry(inputs, "author")
+        year = self._get_entry(inputs, "year")
+        isbn = self._get_entry(inputs, "ISBN")
+
+        #setting up the querry
+        query = "INSERT INTO book" \
+                "(title, author, 'year', isbn)" \
+                " VALUES (?, ?, ?, ?)"
+
+        #setting up the connection
+        con = sql.connect(self._dbName)
+        try:
+            cur = con.cursor()
+            cur.execute(query, (title, author, year, isbn))
+            con.commit()
+        except Exception:
+            #it failed
+            return False
+        finally:
+            con.close()
+
+        return True
+
--- bookshop/user_interface.py
+++ bookshop/user_interface.py
@@ -10,6 +10,8 @@ The user can :
 """
 
 from tkinter import *
+from tkinter.messagebox import *
+import back_end as be
 
 class BookshopGUI:
 
@@ -19,9 +21,20 @@ class BookshopGUI:
         :param master:      The Tk object that we use to build our GUI
         """
         self.master = master
-        self.init_objects()
+        self._db = be.Database("bookshop.db")
 
-    def init_objects(self):
+        self._vars = {
+            "title" : StringVar(),
+            "ISBN" : StringVar(),
+            "author" : StringVar(),
+            "year" : StringVar()
+        }
+
+        self._list_box = Listbox(master=self.master, height=6, width=35)
+
+        self._init_objects()
+
+    def _init_objects(self):
         """
             Initialises the initial states of the objects from the GUI
         :return:    -
@@ -41,42 +54,38 @@ class BookshopGUI:
         lb_ISBN.grid(row=1, column=2)
 
         # setting up edit boxes
-        var_title = StringVar()
-        bx_title = Entry(master=self.master, textvariable=var_title)
+
+        bx_title = Entry(master=self.master, textvariable=self._vars["title"])
         bx_title.grid(row=0, column=1)
 
-        var_year = StringVar()
-        bx_year= Entry(master=self.master, textvariable=var_year)
+        bx_year= Entry(master=self.master, textvariable=self._vars["year"])
         bx_year.grid(row=1, column=1)
 
-        var_author = StringVar()
-        bx_author = Entry(master=self.master, textvariable=var_author)
+        bx_author = Entry(master=self.master, textvariable=self._vars["author"])
         bx_author.grid(row=0, column=3)
 
-        var_ISBN = StringVar()
-        bx_ISBN = Entry(master=self.master, textvariable=var_ISBN)
+        bx_ISBN = Entry(master=self.master, textvariable=self._vars["ISBN"])
         bx_ISBN.grid(row=1, column=3)
 
         # adding the listbox
-        list_box = Listbox(master=self.master, height=6, width=35)
-        list_box.grid(row=2, column=0, rowspan=6, columnspan=2)
+        self._list_box.grid(row=2, column=0, rowspan=6, columnspan=2)
 
         #creating the scrollbar
         scroll_bar = Scrollbar()
         scroll_bar.grid(row=2, column=2, rowspan=6)
 
         #connecting the listbox and the scrollbar
-        list_box.configure(yscrollcommand=scroll_bar.set)
-        scroll_bar.configure(command=list_box.yview )
+        self._list_box.configure(yscrollcommand=scroll_bar.set)
+        scroll_bar.configure(command=self._list_box.yview )
 
         #setting up the buttons
-        bt_view = Button(master=self.master, text="View all", width=12)
+        bt_view = Button(master=self.master, text="View all", width=12, command=self.view_all)
         bt_view.grid(row=2, column=3)
 
         bt_search = Button(master=self.master, text="Search entry", width=12)
         bt_search.grid(row=3, column=3)
 
-        bt_add = Button(master=self.master, text="Add entry", width=12)
+        bt_add = Button(master=self.master, text="Add entry", width=12, command=self.insert_entry)
         bt_add.grid(row=4, column=3)
 
         bt_update = Button(master=self.master, text="Update", width=12)
@@ -88,6 +97,78 @@ class BookshopGUI:
         bt_close = Button(master=self.master, text="Close", width=12)
         bt_close.grid(row=7, column=3)
 
+    def _print_message(self, msg, error = False):
+        """
+
+        :param msg:         The message to print
+        :param error:       True - if it is an error message
+                            False - otherwise
+        :return:
+        """
+        showerror("ERROR", msg)
+
+    def _reset_fields(self):
+        """
+            function that resets the input fields
+        :return:
+        """
+        for key in self._vars:
+            self._vars[key].set("")
+
+    def _update_list_box(self, new_list):
+        """
+            Updates the listbox with the contents of the new list
+        :param new_list:        The new contents to be written in the listbox
+        :return:                -
+        """
+        self._list_box.delete(0) #clearing the list
+
+        #putting the new elements in
+        for entry in new_list:
+            string = "(" + str(entry[0]) + ") " + \
+                str(entry[1]) + ", " + \
+                str(entry[2]) + ", " + \
+                str(entry[3]) + "," + \
+                str(entry[4])
+            self._list_box.insert(END, string)
+
+    def view_all(self):
+        """
+            uses the Database object to read all the entries and then display them
+        in the list box
+        :return:    -
+        """
+        entries = self._db.fetch()
+        self._update_list_box(new_list=entries)
+
+    def insert_entry(self):
+        """
+            Processes the inputs and sends them to the db
+        :return: -
+        """
+        self._print_message("This is just a test message")
+        to_insert = dict()
+        try:
+            year = int(self._vars["year"].get())
+            to_insert["year"] = year
+        except ValueError:
+            print("error")
+            return
+
+        for key in self._vars:
+            if key != "year":
+                if self._vars[key].get() != "":
+                    to_insert[key] = self._vars[key].get()
+                else:
+                    print("error")
+                    return
+
+        success = self._db.insert(to_insert)
+
+        if (success):
+            self._reset_fields()
+        else:
+            print("error")
 
 if __name__ == "__main__":
     # I run the app
